9b6f1cb0dcf93b1bdc19e95b3d143b4147230c97
VadimSvistunov <vadim27112001@gmail.com>
2021-12-21 18:34:46 +0300

    configuration 22 version

--- DependencyInjectionContainerLibrary/DependencyConfiguration.cs
+++ DependencyInjectionContainerLibrary/DependencyConfiguration.cs
@@ -9,7 +9,13 @@ namespace DependencyInjectionContainerLibrary
         private readonly Dictionary<Type, List<Implementation> > _configuration = new ();
         
         public void Register<TType, TImplementation>(bool isSingleton = true) where TType: class
-            
+            where TImplementation: TType
+        {
+            if (!_configuration.ContainsKey(typeof(TType)))
+            { 
+                _configuration[typeof(TType)] = new List<Implementation>();
+            }
+            _configuration[typeof(TType)].Add(new Implementation(typeof(TImplementation), isSingleton));
         }
         
         public void Register(Type dependency, Type implementation, bool isSingleton = true)
